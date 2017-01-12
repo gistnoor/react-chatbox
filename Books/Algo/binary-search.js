cb1afdfa0ae4442ad3058d566733607ca0572980
Noor <gist.asifhasannoor@gmail.com>
2017-01-12 20:16:20 +0600

    Delete binary-search.js

--- Books/Algo/binary-search.js
+++
@@ -1,9 +0,0 @@
-Array.prototype.binaryserach = function(o) {
-    var l = 0, u = this.length, m;
-    while( l <= u) {
-        if ( o > this[m = Math.floor((l + u) / 2)] ) l = m + 1;
-        else u = (o == this[m]) ? -2 : m - 1; 
-    }
-    return (u == 2) ? m : -1;
-}
-
